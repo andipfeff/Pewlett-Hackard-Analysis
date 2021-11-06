4ce6e3305e5750511e7ad2768061f20e5a315173
Pfeffer <pfeffer.andrea@mayo.edu>
2021-11-06 12:23:55 -0500

    adding updated query file for challenge

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,3 +34,23 @@ Order By COUNT(ut.emp_no) DESC;
 
 Select * FROM retiring_titles
 
+--write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM Employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+Order By emp_no;
+
+SELECT * FROM mentorship_eligibility
+SELECT count(*) FROM mentorship_eligibility
